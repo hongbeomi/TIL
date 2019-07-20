e895adc1330553cd3cdd40e18a54e57742a6cd04
hongbeomAhn <dks3963@naver.com>
2019-07-20 16:20:50 +0900

    6장 StopWatch App add

--- Android/안드로이드 생존코딩 - 코틀린 편/Chapter6/StopWatch/app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ Android/안드로이드 생존코딩 - 코틀린 편/Chapter6/StopWatch/app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -9,9 +9,9 @@ import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
 
-    private var time = 0
-    private var isRunning = false
-    private var timerTask: Timer? = null
+    private var time = 0 // 시간을 계산할 변수 0으로 초기화
+    private var isRunning = false // 타이머가 동작 중인지 검사하는 변수
+    private var timerTask: Timer? = null // null을 허용하는 Timer 타입의 변수 선언
     private var lap = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         fab.setOnClickListener {
-            isRunning = !isRunning
+            isRunning = !isRunning // 타이머를 시작한다는 뜻을 알려줌
 
             if (isRunning) {
                 start()
@@ -38,13 +38,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun start() {
-        fab.setImageResource(R.drawable.ic_pause_black_24dp)
+        fab.setImageResource(R.drawable.ic_pause_black_24dp) // 시작버튼을 눌렀을 때 버튼 모양 변화
 
-        timerTask = timer(period = 10) {
+        timerTask = timer(period = 10) { // 0.01초 마다 변수를 증가시키며 UI 갱신
             time++
             val sec = time / 100
             val milli = time % 100
-            runOnUiThread {
+            runOnUiThread { // 워커 스레드로 동작하며 UI 조작을 가능하게 함.
                 secTextView.text = "$sec"
                 milliTextView.text = "$milli"
             }
@@ -53,21 +53,22 @@ class MainActivity : AppCompatActivity() {
 
     private fun pause() {
         fab.setImageResource(R.drawable.ic_play_arrow_black_24dp)
-        timerTask?.cancel()
+        timerTask?.cancel() // 실행 중인 타이머가 있다면 타이머 취소
     }
 
+    // 레코드 시간을 기록해줌.
     private fun recordLapTime() {
-        val lapTime = this.time
+        val lapTime = this.time // 현재 시간을 저장
         val textView = TextView(this)
-        textView.text = "$lap LAP : ${lapTime / 100}.${lapTime % 100}"
+        textView.text = "$lap LAP : ${lapTime / 100}.${lapTime % 100}" //
 
-        // 맨 위에 랩타임 추가
+        // 스크롤뷰 안의 레이아웃의 맨 위에 랩타임 추가
         lapLayout.addView(textView, 0)
         lap++
     }
 
     private fun reset() {
-        timerTask?.cancel()
+        timerTask?.cancel() // 실행 중인 타이머가 있다면 취소
 
         // 모든 변수 초기화
         time = 0
